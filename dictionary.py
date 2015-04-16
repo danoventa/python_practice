eac59ce33149dd9cdcdf02eb510ce39c67b68d61
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-15 21:59:25 -0500

    initial commit

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+import sys
+import os 
+
+DIR = "./tmp/collat_test/"
+
+def parse_file():
+	os.chdir(DIR)
+	for root, dirs, files in os.walk("."):
+		for file in files:
+			if file.endswith(".txt"):
+				f = open(file, 'r')
+        		content = f.read()
+        		parse_words(f.read())
+
+def parse_words(readFile):
+	primatives = [];
+	undefinedWords = [];
+	line = ""
+	for lines in readFile.readlines():
+		print(lines)		
+		for word in lines.split(' '):
+			yield word
+			print(word)
+		
+def main(): 
+	parse_file()
+	pass     		
+
+if __name__ == '__main__':
+	sys.exit(main())
