a760399c5dcd7477bff2a9dbca5f8b23fd51862e
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-18 22:00:23 -0500

    New file, from beginners course in python. Variables, loops (while for).

---
+++ Practice_001.py
@@ -0,0 +1,33 @@
+
+
+print('Hello World')
+print(4**2)
+
+exVar = 100
+print(exVar)
+opVar = exVar / 5.3
+print(opVar)
+
+condition = 1
+while condition < 10:
+	print(condition)
+	
+	#condition = condition + 1
+	'''
+		multiple lines
+		can go here
+	'''
+	
+	condition += 1
+	
+condition = 5
+while condition < 15:
+	print('true')	
+	condition += 1	
+	
+exampleList = [1, 6, 3, 4, 6, 14, 4, 90]	
+for thing in exampleList:
+	print(thing)
+
+for x in range(1, 11):
+	print(x)
