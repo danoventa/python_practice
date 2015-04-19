12cc4c58855eed74c8f2ba73aa25e6a26269d2da
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-19 16:53:20 -0500

    Exceptions try and except style.

--- Practice_005.py
+++ Practice_005.py
@@ -12,11 +12,36 @@ import statistics as s
 import exampleModule
 exList = [2, 2, 4, 5, 6, 2, 4, 6, 7, 3]
 
-exampleModule.exampleFunc('test')
+# mutable can be changed (list)
+# immutable cannot be changed (tuple)
 
+def example():
+	return 15, 19
+	
+a, b = example()
+
+print (a)
+print(b)
+
+
+exampleModule.exampleFunc('yay it worked!')
+
+# try and except exceptions can be stacked. 
+try:
+	name = input('what is your name?')
+	#print('Running the try...')
+	print(str(name))
+
+except TypeError as t:
+	print(str(t))
+
+except NameError as n:
+	print(str(n))
+	
+except Exception as e:
+	print(str(e))
 
 # Statistics, and input
-
 x = s.mean(exList)
 print(x)
 
