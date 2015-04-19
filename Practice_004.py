c85ba831a39078bac323657a2e9b11d31b32cd1d
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-19 16:28:16 -0500

    Added new file with classes module of course.

---
+++ Practice_004.py
@@ -0,0 +1,21 @@
+class calc:
+	def add(x, y):
+		answer = x+y
+		print(answer)
+	
+	def sub (x, y):
+		answer = x-y
+		print(answer)
+		
+	def mult(x, y):
+		answer = x*y
+		print(answer)
+	
+	def div(x, y):
+		answer = x/y
+		print(answer)
+		
+calc.add(5, 2)
+calc.sub(4, 2)
+calc.mult(4, 4)
+calc.div(5, 2)
