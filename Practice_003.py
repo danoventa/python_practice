55197550b61fef3d1180ff3293ae723be51bdd07
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-19 15:15:58 -0500

    Write and read files. Using split and readlines for the files reading. Also adding new text files printed.

--- Practice_003.py
+++ Practice_003.py
@@ -1,5 +1,22 @@
+#write to a file
 writeMe = 'Example text'
 
 saveFile = open('exampleWrite.txt', 'w')
 saveFile.write(writeMe)
 saveFile.close()
+
+#append to a file
+appendMe = 'Some more Text'
+
+saveFile = open('exampleFile.txt', 'a')
+saveFile.write(appendMe)
+saveFile.close()
+
+readMe = open('exampleFile.txt', 'r').read()
+print(readMe)
+
+splitMe = readMe.split('\n')
+print(splitMe)
+
+readMe2 = open('exampleFile.txt', 'r').readlines()
+print(readMe2)
