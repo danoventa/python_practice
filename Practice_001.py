574fa6df2084d79de4ff34fc61e1a571b2af0ad8
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-04-18 22:13:51 -0500

    If elif else moduels covered.

--- Practice_001.py
+++ Practice_001.py
@@ -1,5 +1,3 @@
-
-
 print('Hello World')
 print(4**2)
 
@@ -31,3 +29,39 @@ for thing in exampleList:
 
 for x in range(1, 11):
 	print(x)
+	
+x = 2
+y = 7
+z = 10
+
+if x > y:
+	print(x, 'is greater than', y)
+if x < y:
+	print(x, 'is less than', y)
+if x==y:
+	print(x, 'is equal to', y)
+	
+if x <= y: 
+	print(x, 'is less than or equal to', y)
+
+if z > y > x:
+	print(z, 'is greater than', y, 'which is greater than', x)
+
+x = 3
+y = 6
+
+if x < y:
+	print(x, 'is less than', y)
+if x > y:
+	print(x, 'is greater than', y)
+else:
+	print(x, 'is not greater than', y)
+	
+if x > y:
+	print('x is greater than y')
+elif x < z:
+	print(x,'is less than', z)
+else:
+	print('nothing was the case')
+	
+
