e3c94daa5e9ea9fca348c16c2050fa8726ba5e6f
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-03 18:20:24 -0500

    Able to get only the letter without the period from the line

--- dictionary.py
+++ dictionary.py
@@ -4,29 +4,35 @@ import os
 
 DIR = "./tmp/collat_test/"
 
+def to_defined(toBeDefined):
+	print(toBeDefined)
+	
+def already_defined(defined):
+	print(defined)
+
 def parse_file():
 	os.chdir(DIR)
 	for root, dirs, files in os.walk("."):
 		for file in files:
 			if file.endswith(".txt"):
+				toBeDefined = [];
+				defined = [];
 				fi = open(file, 'r').read()
 				content2 = fi.split('\n')
 				print(content2)
 				for line in content2:
 					print(line)
-					if line == ".":
+					if "." in line:
+						line.split(" ")
+						defined.append(line[0])
+						already_defined(defined)
 						print('is definition')
-				
+					else:
+						toBeDefined.append(line)
+						to_defined(toBeDefined)
+						print('undefined')
+				print ("finished the line loop")
 
-def parse_words(readFile):
-	primatives = [];
-	undefinedWords = [];
-	line = ""
-	for lines in readFile.splitlines():
-		print(lines)		
-		for word in lines.split(' '):
-			yield word
-			print(word)
 		
 def main(): 
 	parse_file()
