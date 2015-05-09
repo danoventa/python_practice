c2059e7646e0d01c24890ad1248b831c14a1dd46
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-08 21:33:55 -0500

    Second set of exercises.

--- Practice_007.py
+++ Practice_007.py
@@ -13,3 +13,29 @@ list2 = ["Fifth Element", "Gattaca"]
 list1 = list1 + list2
 list1.append("white fang")
 print(list1)
+
+print(20 - 10 + 3 + 15-5  + (3 * 15))
+
+FirstName = input("insert first name ")
+LastName = input ("Insert last name ")
+
+
+print("Hello there, %s %s" %(FirstName, LastName))
+
+for i in range(0, 29, 2):
+	print(i)
+	
+day = "monday"
+if day == "monday":
+	print ("today it will rain")
+else:
+	print ("today is sunny")
+	
+weight = 200.0
+moonweight = 200.0 * (1.0/6.0)
+gain = 1.0 * (1.0/6.0)
+for i in range(0, 10):
+	weight = weight + 1
+	moonweight = moonweight + gain
+	print (weight)
+	print (moonweight)
