d18bf954b06409115b1e4221427b6f8b5589931b
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-08 20:40:13 -0500

    Exercise 1

--- Practice_007.py
+++ Practice_007.py
@@ -3,3 +3,13 @@ print("hello, world!")
 num = 20
 age = "I am %s years old."
 print(age %num)
+
+tuplefirst = (0, 1, 2, 3)
+""" tuples are not rewriteable."""
+
+list1 = ["When We Were Young", "Yellow"]
+list2 = ["Fifth Element", "Gattaca"]
+
+list1 = list1 + list2
+list1.append("white fang")
+print(list1)
